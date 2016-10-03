42f6079f935361e2fc2969e6090f91c689095401
evalle <shmarnev@gmail.com>
2016-10-03 17:00:51 +0200

    rewrite functions

--- montheorem.py
+++ montheorem.py
@@ -6,14 +6,15 @@ keystr = "methinks it is like a weasel"
 resultstr = list()
 quotelen = len(keystr)
 
-def string_generator(resultstr):
+def string_generator():
     """ generate random string and add it to resultstr """
+    chars = string.ascii_lowercase+' '
     for l in range(quotelen):
-        resultstr.append(random.choice(string.ascii_lowercase+' '))
+        resultstr.append(random.choice(chars))
     resultstr =  ''.join(resultstr)
     return resultstr
 
-def score(resultstr, keystr):
+def score():
     """ compare keystr with resultstr and give the score for each compare """ 
     scorenum = 0
     keystr = keystr.split()
@@ -22,15 +23,16 @@ def score(resultstr, keystr):
         if i == j:
             scorenum += 1
     scorecount = (scorenum / quotelen) * 100
+    print(keystr)
     print(resultstr)
     print(scorecount)
 
-def iterator(resultstr, keystr):
+def iterator():
     """ run string_generator and compare it's result with keystring 1000 times """ 
-    i = 1000
+    i = 3
     while i > 0:
-        string_generator(resultstr)
-        score(resultstr, keystr)
+        string_generator()
+        score()
         i -= 1
 
-iterator(resultstr, keystr)
+iterator()

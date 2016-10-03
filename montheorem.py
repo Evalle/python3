b92eec55d327320b77a9b81b9c652f1b8e42747a
evalle <shmarnev@gmail.com>
2016-10-03 13:55:18 +0200

    Refactoring

--- montheorem.py
+++ montheorem.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 
-import random
-import sys
+import random, sys, string
 
-keystr = "methinks it is like a weasel"
+keyStr = "methinks it is like a weasel"
 resultstr = list()
+quoteLen = len(keyStr)
 
 def string_generator(resultstr):
     """ generate random string and add it to resultstr (global) """
     for l in range(27):
-        resultstr.append(random.choice(" abcdefghijklmnopqrstuvwxyz"))
+        resultstr.append(random.choice(string.ascii_lowercase+' '))
     resultstr =  ''.join(resultstr)
     return resultstr
 
-def comparator(resultstr, keystr):
-    """ compare keystr with resultstr """ 
-    resultstr = ''.join(resultstr)
-    if resultstr == keystr:
-        print("100%!")
-        sys.exit(0)
-    else:
-        print(resultstr)
+def score(resultstr, keyStr):
+    """ compare keystr with resultstr and give the score for each compare """ 
+    resultstr = 
+    for i,k in zip(
 
-def iterator(resultstr, keystr):
+
+def iterator(resultstr, keyStr):
     """ run string_generator and compare it's result with keystring 1000 times """ 
     i = 1000
     while i > 0:

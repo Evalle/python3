53f811da166fd8930802122e79498a078966d3c7
evalle <shmarnev@gmail.com>
2016-10-04 13:35:42 +0200

    Typo in original task, change the code accordingly

--- montheorem.py
+++ montheorem.py
@@ -3,22 +3,23 @@ import random, sys, string
 
 quote = "methinks it is like a weasel"
 
-def generator():
+def generator(quote):
     """ generate random line """ 
     ranline = list()
-    for i in range(27):
+    for i in range(len(quote)):
         ranline.append(random.choice(string.ascii_lowercase+' '))
     return ''.join(ranline)
 
 def score(quote):
     """ compare random line with quote and mark it """ 
-    ranline = generator()
+    ranline = generator(quote)
     scorenum = 0
     for i,k in zip(quote,ranline):
         print(i,k)
         if i == k:
             scorenum += 1
-    score_percent = (scorenum * 27) / 100
+    score_percent = (scorenum / len(quote)) * 100
+    print(scorenum)
     return score_percent
 
 print(score(quote))

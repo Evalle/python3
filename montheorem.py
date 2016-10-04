edfc861acd6346359c490dfbb0bbb99cfe6d0606
evalle <shmarnev@gmail.com>
2016-10-04 07:02:36 +0200

    Total rewriting

--- montheorem.py
+++ montheorem.py
@@ -1,38 +1,14 @@
 #!/usr/bin/env python3
-
 import random, sys, string
 
-keystr = "methinks it is like a weasel"
-resultstr = list()
-quotelen = len(keystr)
+quote = "methinks it is like a weasel"
+quotelen = len(quote)
 
-def string_generator():
-    """ generate random string and add it to resultstr """
-    chars = string.ascii_lowercase+' '
-    for l in range(quotelen):
-        resultstr.append(random.choice(chars))
-    resultstr =  ''.join(resultstr)
-    return resultstr
+def generator(quotalen):
+    """ generate random line """ 
+    ranline = list()
+    for i in range(quotelen):
+        ranline.append(random.choice(string.ascii_lowercase+' '))
+    return ''.join(ranline)
 
-def score():
-    """ compare keystr with resultstr and give the score for each compare """ 
-    scorenum = 0
-    keystr = keystr.split()
-    resultstr = (''.join(resultstr)).split()
-    for i,j in zip(keystr, resultstr):
-        if i == j:
-            scorenum += 1
-    scorecount = (scorenum / quotelen) * 100
-    print(keystr)
-    print(resultstr)
-    print(scorecount)
-
-def iterator():
-    """ run string_generator and compare it's result with keystring 1000 times """ 
-    i = 3
-    while i > 0:
-        string_generator()
-        score()
-        i -= 1
-
-iterator()
+print(generator(quotelen))

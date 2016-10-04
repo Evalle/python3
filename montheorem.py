c2a9ccf5dde585d0db544cc558c737c0fc57e4c8
evalle <shmarnev@gmail.com>
2016-10-04 08:59:51 +0200

    Add score_percent as return of score(quote) function

--- montheorem.py
+++ montheorem.py
@@ -2,24 +2,23 @@
 import random, sys, string
 
 quote = "methinks it is like a weasel"
-quotelen = len(quote)
 
-def generator(quotalen):
+def generator():
     """ generate random line """ 
     ranline = list()
-    for i in range(quotelen):
+    for i in range(27):
         ranline.append(random.choice(string.ascii_lowercase+' '))
     return ''.join(ranline)
 
-def score(quote,quotelen):
+def score(quote):
     """ compare random line with quote and mark it """ 
-    ranline = generator(quotelen)
+    ranline = generator()
     scorenum = 0
     for i,k in zip(quote,ranline):
+        print(i,k)
         if i == k:
             scorenum += 1
-    print(quote)
-    print(ranline)
-    return scorenum
+    score_percent = (scorenum * 27) / 100
+    return score_percent
 
-print(score(quote,quotelen))
+print(score(quote))

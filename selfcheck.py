875374ffb95dbdc5e87b05c1f1543ce0949b7862
evalle <shmarnev@gmail.com>
2016-10-05 15:17:28 +0200

    Add GCD(Euclid) function

--- selfcheck.py
+++ selfcheck.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
+def gcd(m, n):
+    while m % n != 0:
+        old_m = m
+        old_n = n
+
+        m = old_n
+        n = old_m % old_n
+    return n
+
+
 class Fraction:
-    
+
     def __init__(self, top, bottom):
         self.chis = top
         self.znam = bottom
@@ -9,11 +19,12 @@ class Fraction:
         return str(self.chis) + '/' + str(self.znam)
 
     def __add__(self, other_fr):
-        new_chis = self.chis*other_fr.znam + other_fr.chis*self.znam
-        new_znam = self.znam*other_fr.znam
+        new_chis = self.chis * other_fr.znam + other_fr.chis * self.znam
+        new_znam = self.znam * other_fr.znam
         return Fraction(new_chis, new_znam)
 
-f1 = Fraction(1,4)
-f2 = Fraction(1,2)
+
+f1 = Fraction(1, 4)
+f2 = Fraction(1, 2)
 f3 = f1 + f2
 print(f3)

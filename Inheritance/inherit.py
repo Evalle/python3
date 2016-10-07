313c5faa77c466f65679b707357bcd4a3727a626
evalle <shmarnev@gmail.com>
2016-10-07 15:43:45 +0200

    Add BinaryGate class

--- Inheritance/inherit.py
+++ Inheritance/inherit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-class LogicGate:
+
+class LogicGate(object):
 
     def __init__(self, n):
         self.label = n
@@ -10,5 +11,20 @@ class LogicGate:
         return self.label
 
     def get_output(self):
-        self.output = self.perform_gate_logic()
+        self.output = self.performGateLogic()
         return self.output
+
+
+class BinaryGate(LogicGate):
+
+    def __init__(self, n):
+        LogicGate.__init__(self, n)
+
+        self.pinA = None
+        self.pinB = None
+
+    def get_pinA(self):
+        return int(input("Enter Pin A input for gate " + self.get_label() + "-->"))
+
+    def get_pinB(self):
+        return int(input("Enter Pin B input for gate " + self.get_label() + "-->"))

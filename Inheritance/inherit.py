97a37378c277bb6a1e19ddfececaa99f125217ce
evalle <shmarnev@gmail.com>
2016-10-07 12:51:39 +0200

    Add an example of ingeritance

---
+++ Inheritance/inherit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+class LogicGate:
+
+    def __init__(self, n):
+        self.label = n
+        self.output = None
+
+    def get_label(self):
+        return self.label
+
+    def get_output(self):
+        self.output = self.perform_gate_logic()
+        return self.output

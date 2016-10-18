b44a026ce2f0b47adca2cde23afb9a0528d3149a
evalle <shmarnev@gmail.com>
2016-10-18 14:13:18 +0200

    Add poc4 O(n) solution

---
+++ Anagram_detection/poc4.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Anagram detection example, POC #4
+# O(n) because of sorting operations
+
+def anagram_solution(s1, s2):
+    c1 = [0] * 26
+    c2 = [0] * 26
+
+    for i in range(len(s1)):
+        pos = ord(s1[i]) - ord('a')
+        c1[pos] = c1[pos] + 1
+
+    for i in range(len(s2)):
+        pos = ord(s2[i]) - ord('a')
+        c2[pos] = c2[pos] + 1
+
+    j = 0
+    stillOK = True
+    while j < 26 and stillOK:
+        if c1[j] == c2[j]:
+            j = j + 1
+        else:
+            stillOK = False
+    return stillOK
+
+print(anagram_solution('apple', 'pleap'))

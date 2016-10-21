d4ba41160a97390e9403200f0631450e4f8ec628
evalle <shmarnev@gmail.com>
2016-10-21 11:11:36 +0200

    Add Stack class example

---
+++ Basic_Data_Structures/stack.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Basic implementation of stack in python3
+
+
+class Stack(object):
+
+    def __init__(self):
+        self.items = list()
+
+    def isempty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)

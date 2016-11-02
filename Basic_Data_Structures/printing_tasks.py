dbfd169f2ad88ebbb7dc1dbc2e5065e8a4ad5131
evalle <shmarnev@gmail.com>
2016-11-02 17:24:45 +0100

    Add Task class

--- Basic_Data_Structures/printing_tasks.py
+++ Basic_Data_Structures/printing_tasks.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import random
+
 class Printer(object):
 
     def __init__(self, ppm):
@@ -23,3 +25,19 @@ class Printer(object):
     def start_next(self, new_task):
         self.current_task = newtask
         self.time_remaining = newtask.get_pages() * 60 / self.pagerate
+
+
+class Task(object):
+
+    def __init__(self, time):
+        self.timestamp = time
+        self.pages = random.randrange(1, 21)
+
+    def get_stamp(self):
+        return self.timestamp
+
+    def get_pages(self):
+        return self.pages
+
+    def wait_time(self, currenttime):
+        return currenttime - self.timestamp

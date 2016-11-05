c7bd583326a0915c08d0d71d1633cbf58e79876a
evalle <shmarnev@gmail.com>
2016-11-05 13:08:50 +0100

    Add size function (traversal method)

---
+++ unordered_list/unordered_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from node_example import Node
+
+class UnorderedList(object):
+    
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0 
+        while current != None:
+            count += 1
+            current = current.get_next()
+
+        return count

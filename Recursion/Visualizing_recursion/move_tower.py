08464a9141c1f6cbfd40d4e78a70167c6c287e43
evalle <shmarnev@gmail.com>
2016-11-16 13:43:48 +0100

    Add Hanoi Tower

---
+++ Recursion/Visualizing_recursion/move_tower.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Hanoi Tower
+
+
+def moveTower(height, fromPole, toPole, withPole):
+    if height >= 1:
+        moveTower(height - 1, fromPole, withPole, toPole)
+        moveDisk(fromPole, toPole)
+        moveTower(height-1, withPole, toPole, fromPole)
+
+
+def moveDisk(fp, tp):
+    print("moving disk from", fp, "to", tp)
+
+moveTower(3, "A", "B", "C")

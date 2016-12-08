0ecd698cfb8da3c74c545893306eecfebac87e00
evalle <shmarnev@gmail.com>
2016-12-08 16:11:26 +0100

    Add an implementation of the recursive binary search function

---
+++ Search/binsearch_recursive.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Simple implementation of the recursive binary search
+
+
+def binary_search(lst, item):
+
+    if len(lst) == 0:
+        return False
+    else:
+        midpoint = len(lst) // 2
+        if lst[midpoint] == item:
+            return True
+        else:
+            if item < lst[midpoint]:
+                return binary_search(lst[:midpoint], item)
+            else:
+                return binary_search(lst[midpoint + 1:], item)

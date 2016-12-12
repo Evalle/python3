5dc2b2c13bf4e2855f5e73704a2e79ed3dd9ee84
evalle <shmarnev@gmail.com>
2016-12-12 23:04:43 +0100

    Fix stop argument in first range call

--- Sort/bubble.py
+++ Sort/bubble.py
@@ -3,9 +3,9 @@
 # Simple example of bubble sort in Python 3
 
 
-def bubble(lst):
+def bubblesort(lst):
 
-    for passnum in range(len(lst) - 1, 0, 1):
+    for passnum in range(len(lst) - 1, 0, -1):
         for i in range(passnum):
             if lst[i] > lst[i + 1]:
                 temp = lst[i]

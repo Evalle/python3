3a030b1f2cad231f76861089d05cf8a3a4e1cb94
evalle <shmarnev@gmail.com>
2016-12-12 22:26:14 +0100

    Fix identation error

--- Sort/bubble.py
+++ Sort/bubble.py
@@ -6,7 +6,7 @@ def bubble(lst):
 
     for passnum in range(len(lst) - 1, 0, 1):
         for i in range(passnum):
-            if lst[i] > lst[i + 1]
-            temp = lst[i]
-            lst[i] = lst[i + 1]
-            lst[i + 1] = temp
+            if lst[i] > lst[i + 1]:
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp

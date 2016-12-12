9df6d27ab437ddba3e50e7441b0bf9f78c2389c9
evalle <shmarnev@gmail.com>
2016-12-12 22:19:32 +0100

    Add bubble sort example

---
+++ Sort/bubble.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3 
+
+# Simple example of bubble sort in Python 3
+
+def bubble(lst):
+
+    for passnum in range(len(lst) - 1, 0, 1):
+        for i in range(passnum):
+            if lst[i] > lst[i + 1]
+            temp = lst[i]
+            lst[i] = lst[i + 1]
+            lst[i + 1] = temp

081ee176311c8df1e67a971dd7ebd46f08248270
evalle <shmarnev@gmail.com>
2017-01-13 17:57:51 +0100

    Add non-pythonic way of sloving reverse string problem

--- 100days-of-code/reverse.py
+++ 100days-of-code/reverse.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
-print("Pleae enter your string")
-user_input = (list(input("> ")))
-user_input.reverse()
-user_input = (''.join(user_input))
-print("here is your reversed string: %s" % user_input)
+# python way:
+
+def python_way():
+	print("Pleae enter your string")
+	user_input = (list(input("> ")))
+	user_input.reverse()
+	user_input = (''.join(user_input))
+	print("here is your reversed string: %s" % user_input)
+
+
+def non_python_way():
+	print("Please enter your string")
+	user_input = input("> ")
+	user_input_reversed = list()
+	for i in user_input[::-1]:
+		user_input_reversed.append(i)
+	print("here is your reversed string: %s" % ''.join(user_input_reversed))
+
+def main():
+	# python_way()
+	non_python_way()
+
+main()

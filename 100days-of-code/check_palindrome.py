bd6caddaa5e6e4280168009fde37a9f1903ed484
evalle <shmarnev@gmail.com>
2017-01-18 11:32:00 +0100

    Add check palindrome script

--- 100days-of-code/check_palindrome.py
+++ 100days-of-code/check_palindrome.py
@@ -5,4 +5,20 @@ Check if Palindrome - Checks if the string entered by the user is a palindrome. 
 reads the same forwards as backwards like “racecar”
 '''
 
-user_input = ('> ')
+user_input = (input('> ').lower())
+
+
+def reverse_function(inp):
+    user_input_list = list(user_input)
+    reverse = ''.join((user_input_list[::-1]))
+    return reverse
+
+
+def main(inp):
+    if user_input == reverse_function(inp):
+        print('Palindrome!')
+    else:
+        print('Not Palindrome!')
+
+
+main(user_input)

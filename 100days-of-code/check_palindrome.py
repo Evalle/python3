54cb122835c225702ea57821b03e0183e2fdaf89
evalle <shmarnev@gmail.com>
2017-01-18 10:55:25 +0100

    Add check palindrome script

---
+++ 100days-of-code/check_palindrome.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+''' 
+Check if Palindrome - Checks if the string entered by the user is a palindrome. That is that it 
+reads the same forwards as backwards like “racecar”
+'''
+
+user_input = ('> ')

1d34400a7e5699f83506ca75c4ed8f303b5f8ed7
evalle <shmarnev@gmail.com>
2017-01-18 16:42:30 +0100

    add text_editor script template

---
+++ 100days-of-code/text_editor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''
+Text Editor - Notepad style application that can open, edit, and save text documents. 
+Optional: Add syntax highlighting and other features.
+'''
+
+user_input = input('> 'l)
+
+if ''
